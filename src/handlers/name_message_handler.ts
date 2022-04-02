1801893f4eb6deab08b53f7524a437213d758728
Mikael Porttila <mikael.porttila@gmail.com>
2022-04-02 13:53:52 +0200

    force push deploy

--- src/handlers/name_message_handler.ts
+++ src/handlers/name_message_handler.ts
@@ -20,7 +20,7 @@ export function createHandler(emojiService: EmojiService): MessageHandler {
           bot,
           message.channelId,
           message.id,
-          smartGuyReactionEmoji,
+          smartGuyReactionEmoji
         );
       }
     }
