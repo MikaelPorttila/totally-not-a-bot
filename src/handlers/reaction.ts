062e6525964f1a260e8995e07fbcd099f2815b7a
Mikael Porttila <mikael.porttila@gmail.com>
2022-04-04 17:15:49 +0200

    Added some auto-replies

--- src/handlers/reaction.ts
+++ src/handlers/reaction.ts
@@ -1,4 +1,5 @@
-import { addReaction, Bot, Message } from "../../deps.ts";
+import { addReaction, sendMessage } from "../../deps.ts";
+import type { Bot, Message } from "../../deps.ts";
 import { EmojiService } from "../services/mod.ts";
 import { MessageHandler } from "./types/mod.ts";
 
@@ -16,6 +17,14 @@ export function createHandler(emojiService: EmojiService): MessageHandler {
       for: ["cod", "snarkov", "tarkov", "cs", "stridsåker", "bf"],
       reaction: "🎮",
     },
+    {
+      for: ['bror', 'bruh', 'bro', 'brother', 'broder'],
+      reply: "What's up?"
+    },
+    {
+      for: ['namn'],
+      reply: "Samma efternamn"
+    }
   ];
 
   return async function (
@@ -25,16 +34,34 @@ export function createHandler(emojiService: EmojiService): MessageHandler {
   ) {
     for (const group of reactionTable) {
       if (
-        group.reaction &&
         group.for.some((term) => normalizedMessageWords.some((x) => x === term))
       ) {
         try {
-          await addReaction(
-            bot,
-            message.channelId,
-            message.id,
-            group.reaction,
-          );
+          if(group.reaction) {
+            await addReaction(
+              bot,
+              message.channelId,
+              message.id,
+              group.reaction,
+            );
+          }
+
+          if (group.reply) {
+            await sendMessage(
+              bot,
+              message.channelId,
+              {
+                content: group.reply,
+                messageReference: {
+                  channelId: message.channelId,
+                  guildId: message.guildId,
+                  messageId: message.id,
+                  failIfNotExists: false,
+                },
+              },
+            );
+          }
+
         } catch (err) {
           console.error("[Reaction Handler]", err);
         }
