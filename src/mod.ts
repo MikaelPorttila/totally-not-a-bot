0b836aff015ea243eb6dc1190ac5a3a52c66567c
Mikael Porttila <mikael.porttila@gmail.com>
2022-04-07 22:56:48 +0200

    Trying to make azure happy

--- src/mod.ts
+++ src/mod.ts
@@ -1,4 +1,12 @@
-import { startBot } from "../deps.ts";
+import { startBot, serve } from "../deps.ts";
 import { bot } from "./bot.ts";
 
-await startBot(bot);
+const port = 1993;
+const defaultResponse = new Response('🙈', { status: 200 });
+const handler = (_: Request): Response => {
+  return defaultResponse;
+};
+
+const server = serve(handler, { port });
+const botRunner = startBot(bot);
+await Promise.all([server, botRunner]);
