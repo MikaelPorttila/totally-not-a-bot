8e6651f54edd3bf50223cffd60985dfea17e1447
Mikael Porttila <mikael.porttila@gmail.com>
2022-05-14 11:45:56 +0200

    Added Tag Me command.

---
+++ src/commands/tag_me.ts
@@ -0,0 +1,26 @@
+import {
+    ApplicationCommandTypes,
+    InteractionResponseTypes,
+  } from "../../deps.ts";
+  import type { Bot } from "../../deps.ts";
+  import { createCommand } from "../helpers/command_helper.ts";
+  
+  export function registerCommand() {
+    createCommand({
+      name: "Tag me",
+      description: "Tags the message author in the reply",
+      type: ApplicationCommandTypes.ChatInput,
+      execute: async (bot: Bot, interaction) => {
+        await bot.helpers.sendInteractionResponse(
+          interaction.id,
+          interaction.token,
+          {
+            type: InteractionResponseTypes.ChannelMessageWithSource,
+            data: {
+              content: `<@${interaction.member?.id}>`,
+            },
+          },
+        );
+      },
+    });
+  }
