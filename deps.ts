feb4f66ce1ae4ee22cb33c36f3ec5c9078303d70
Mikael Porttila <mikael.porttila@gmail.com>
2022-09-03 21:08:19 +0200

    revert discorddeno version

--- deps.ts
+++ deps.ts
@@ -10,7 +10,7 @@ export type {
   Interaction,
   Message,
   User,
-} from "https://deno.land/x/discordeno/mod.ts";
+} from "https://deno.land/x/discordeno@13.0.0-rc36/mod.ts";
 
 export {
   addReaction,
@@ -40,14 +40,14 @@ export {
   stopBot,
   unpinMessage,
   upsertApplicationCommand,
-} from "https://deno.land/x/discordeno/mod.ts";
+} from "https://deno.land/x/discordeno@13.0.0-rc36/mod.ts";
 
 /* export { DOMParser } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
 
 export type { Element } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts"; */
 
-export * from "https://deno.land/std@0.117.0/fmt/colors.ts";
+export * from "https://deno.land/std@0.154.0/fmt/colors.ts";
 
-export { serve } from "https://deno.land/std@0.133.0/http/server.ts";
+export { serve } from "https://deno.land/std@0.154.0/http/server.ts";
 
 /* export { gql, request } from "https://deno.land/x/graphql_request/mod.ts"; */
